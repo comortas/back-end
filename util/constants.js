1cdc9c9534586ad2cd78c33bc0017f6058de81bc
unknown <suruthianbucheziyan@gmail.com>
2022-09-22 20:20:01 +0530

    removed unwanted code

--- util/constants.js
+++ util/constants.js
@@ -1,35 +1,6 @@
 
 //App Constants
 module.exports = {
-    Role: {
-        Patient: "patient",
-        Provider: "provider",
-        Admin: "admin",
-        MedicalAssistant: "medicalAssistant",
-        SuperAdmin: "superAdmin",
-        Executive: "executive",
-        Director: "director",
-        System: "system"
-    },
-    Appointment: {
-        ParticipantType: {
-            Patient: 'patient',
-            Provider: 'provider',
-            MedicalAssistant: 'medicalAssistant'
-        }
-    },
-    ApplicationSettings: {
-        PayoutInterval: 'payout_interval',
-        PayoutDelayDays: 'payout_delay_days',
-        PayoutAnchor: 'payout_anchor',
-        ConvenienceFee: 'convenience_fee',
-        CoPayFormula: 'co_pay_formula',
-        SelfPayFormula: 'self_pay_formula'
-    },
-    MDToken: {
-        Name: 'MD_Telehealth_Token',
-        Expiration: 86400 // 24 hours
-    },
     HTTP_STATUS_CODE: {
         SUCCESS: 200,
         BAD_REQUEST: 400,
--- util/message.js
+++ util/message.js
@@ -2,11 +2,6 @@
 //App Constants
 module.exports = {
     ErrorMessage: {
-        EmailAndPhoneNotExist: "Unable to process request when email and phone is empty",
-        FutureAppointmentOnlyAllowed: "You can only create future appointments",
-        FirstNameLastNameEmpty : "Unable to process request when first name and last name is empty",
-        DateTimeEmpty: "Unable to process request when date and time is empty",
-        InvalidURL: "Invalid URL",
         MissingInput: "Missing Input",
         MissingToken : "Missing Token",
         InvalidToken : "Invalid Token",
